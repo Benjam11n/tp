875c11dae58bc2bf0b7e141277b12b2259557484
Benjamin Wang <ben.wang9000@gmail.com>
2025-02-20 23:02:52 +0800

    Add Remark command

---
+++ src/main/java/seedu/address/model/person/Remark.java
@@ -0,0 +1,53 @@
+package seedu.address.model.person;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.util.AppUtil.checkArgument;
+
+public class Remark {
+    public static final String MESSAGE_CONSTRAINTS =
+            "Remark must be less than 100 characters";
+    public final String value;
+
+    /**
+     * Constructs a {@code Name}.
+     *
+     * @param remark A valid remark.
+     */
+    public Remark(String remark) {
+        requireNonNull(remark);
+        checkArgument(isValidRemark(remark), MESSAGE_CONSTRAINTS);
+        this.value = remark;
+    }
+
+    /**
+     * Returns true if a given string is a valid name.
+     */
+    public static boolean isValidRemark(String remark) {
+        return remark != null && !remark.isEmpty() && remark.length() <= 100;
+    }
+
+    @Override
+    public String toString() {
+        return this.value;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof Remark)) {
+            return false;
+        }
+
+        Remark otherRemark = (Remark) other;
+        return this.value.equals(otherRemark.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return this.value.hashCode();
+    }
+}
